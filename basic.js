85d0d99924caffb7813541c8b839f419cec6d8a6
Ansh Goel <138398146+AnshG0el@users.noreply.github.com>
2026-04-03 15:38:33 +0530

    'reverse-function-ad'

--- basic.js
+++ basic.js
@@ -23,33 +23,47 @@
 
 // a.slice(1,2);
 // console.log(a);
-let mode=0;
-btn=document.querySelector("button");
-body=document.querySelector("body");
-btn.addEventListener('click',()=>{
-    if(mode%2==0){
-        console.log("bg is black");
-        body.style.backgroundColor="black";
-        mode++;
-    }
-    else{
-        body.style.backgroundColor="white";
-        mode++;
-    }
+// let mode=0;
+// btn=document.querySelector("button");
+// body=document.querySelector("body");
+// btn.addEventListener('click',()=>{
+//     if(mode%2==0){
+//         console.log("bg is black");
+//         body.style.backgroundColor="black";
+//         mode++;
+//     }
+//     else{
+//         body.style.backgroundColor="white";
+//         mode++;
+//     }
 
-})
+// })
 
 
 // even or odd 
-let val=prompt("enter a number and check odd or even");
-let b=document.querySelector("h1")
-function evenodd(a){
-    if(a%2==0){
-        b.innerText="this is even";
-    }
-    else{
-        b.innerText="this is odd";
-    }
+// let val=prompt("enter a number and check odd or even");
+// let b=document.querySelector("h1")
+// function evenodd(a){
+//     if(a%2==0){
+//         b.innerText="this is even";
+//     }
+//     else{
+//         b.innerText="this is odd";
+//     }
+
+// }
+// evenodd(val);
+// reverse string 
+let str="ansh"
+function rev(str){
+
+    let a=str.split("");
+    console.log(a);
+    a.reverse();
+    // console.log(a);
+    let b=a.join("");
+    console.log(b);
+
 
 }
-evenodd(val);
+rev(str);
