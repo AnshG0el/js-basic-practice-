d3d4d3eab905b7c0f983d5c9936ba5d9a32019c8
Ansh Goel <138398146+AnshG0el@users.noreply.github.com>
2026-04-03 12:01:00 +0530

    normal-code

---
+++ basic.js
@@ -0,0 +1,40 @@
+// // let a=20;
+// // console.log(a)
+// // print all even numbers 0 to 100
+// // for(let i=0;i<=100;i++){
+// //     if(i%2==0){
+// //         console.log(i);
+// //     }
+// // // }
+// // let name=prompt("enter user name");
+// // console.log(` @${name}${name.length}`)
+
+// // let arr=[10,12,34,34,23,56,19];
+// // let sum=0;
+// // for(let i of arr){
+// //      sum=i+sum;
+// // }
+// // console.log(sum/arr.length); 
+
+
+// let a=[1,2,3,4,5];
+// // a.shift()
+
+
+// a.slice(1,2);
+// console.log(a);
+let mode=0;
+btn=document.querySelector("button");
+body=document.querySelector("body");
+btn.addEventListener('click',()=>{
+    if(mode%2==0){
+        console.log("bg is black");
+        body.style.backgroundColor="black";
+        mode++;
+    }
+    else{
+        body.style.backgroundColor="white";
+        mode++;
+    }
+
+})
